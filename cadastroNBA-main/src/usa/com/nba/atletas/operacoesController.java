4af47409624f67eef3cd059ec03490df91bea226
lab04 <lab04@lab4-19>
2024-09-23 22:32:13 -0300

    criando banco de dados e classe conexao

--- cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
+++ cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,27 @@
 package usa.com.nba.atletas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class operacoesController {
+	
+	@FXML
+	public TextField nome;
+	
+	@FXML
+	public TextField sobrenome;
+	
+	
+	@FXML
+	public TextField idade;
+	
+	
+	@FXML
+	public TextField posicao;
+	
+	
+	@FXML
+	public TextField equipe;
+	
 	
 }
